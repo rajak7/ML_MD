f2b36c1e7375f09e7b0224b9adaeaa55293869a9
Ken-ichi Nomura <kenichi.nomura@gmail.com>
2019-05-11 17:12:05 -0700

    code for gr&sq calc

---
+++ stat/stat.f90
@@ -0,0 +1,202 @@
+module stat_mod
+
+  implicit none
+  real(8),parameter :: pi = 4.d0*datan(1.d0)
+  integer,parameter :: NTABLES = 100
+  real(8),parameter :: RCUT = 10d0, DRI = NTABLES/RCUT
+  real(8),parameter :: QCUT = 20d0, DQ = QCUT/NTABLES
+
+  type string_array
+     character(len=:),allocatable :: str
+  end type
+
+  type mdframe
+     character(len=:),allocatable :: filename
+     type(string_array),allocatable :: elems(:)
+     real(8),allocatable :: pos(:,:), v(:,:), f(:,:), q(:)
+     real(8) :: lattice(6)
+     integer :: num_atoms
+  end type
+
+  type analysis_context
+
+     type(string_array),allocatable :: elems(:)
+     real(8),allocatable :: concentration(:), gr(:,:,:), nr(:,:,:), ba(:,:,:,:), sq(:,:,:)
+
+     integer :: num_atoms, num_atom_types
+     real(8) :: volume, lattice(6), kvector(3)
+     integer,allocatable :: num_atoms_per_type(:)
+
+     integer :: num_sample_frames 
+
+  contains
+
+     procedure :: print => print_analysis_context 
+     procedure :: save_stat => save_analysis_results
+
+  end type
+
+contains
+
+  subroutine print_analysis_context(this) 
+     class(analysis_context) :: this
+     character(len=:),allocatable :: name
+     integer :: i
+
+     print'(a)',repeat('-',60)
+     print'(a,i6)','num_atoms: ', this%num_atoms
+     print'(a,3f8.3, 3f8.2)','lattice: ', this%lattice
+     print'(a,f8.3)','volume: ', this%volume
+     print'(a,3f8.3)','k-vector: ', this%kvector
+
+     print'(a, i6)', 'num_elements: ', size(this%elems)
+     print'(a $)', 'elements: '
+     do i = 1, size(this%elems)
+        name = this%elems(i)%str
+        print'(i1, a1, a, a $)',get_index(this%elems, name),'-',adjustl(name), '   '
+     enddo
+     print*
+     print'(a,10i6)', 'num_atoms_per_type: ', this%num_atoms_per_type
+     print'(a,10f8.5)', 'concentration: ', this%concentration
+     print'(a)',repeat('-',60)
+
+  end subroutine
+
+  subroutine save_analysis_results(this) 
+     class(analysis_context) :: this
+     integer :: iunit,ity,jty,k,kk
+     real(8) :: dr, rho, dqk, prefactor, prefactor2
+
+     rho = this%num_atoms/this%volume
+
+     open(newunit=iunit,file='gr.dat',form='formatted')
+
+     !print'(a $)', 'distance'
+     write(unit=iunit,fmt='(a $)') 'distance'
+     do ity=1,size(this%elems)
+     do jty=1,size(this%elems)
+        !print'(a5,i1,a1,i1 $)',',    ',ity,'-',jty
+        write(unit=iunit,fmt='(a5,a5 $)') & 
+           ',    ',this%elems(ity)%str//'-'//this%elems(jty)%str
+     enddo; enddo
+     !print*
+     write(unit=iunit,fmt=*)
+
+     do k=1,size(this%gr,dim=3)
+        !print'(f10.5 $)', k/DRI
+        dr = k/DRI
+        prefactor = 4.d0*pi*dr*dr*rho/DRI
+        write(unit=iunit,fmt='(f12.5 $)') dr
+        do ity=1,size(this%elems)
+        do jty=1,size(this%elems)
+          !print'(f10.1,3x $)', this%gr(ity,jty,k)
+          prefactor2 = this%concentration(jty)*this%num_atoms_per_type(ity)*this%num_sample_frames
+          this%gr(ity,jty,k) = this%gr(ity,jty,k)/(prefactor*prefactor2)
+          write(iunit, fmt='(f12.5,3x $)') this%gr(ity,jty,k)
+        enddo; enddo
+        !print*
+        write(unit=iunit,fmt=*)
+     enddo
+
+     close(iunit)
+
+
+     open(newunit=iunit,file='sq.dat',form='formatted')
+     write(unit=iunit,fmt='(a $)') 'wave_vector '
+     do ity=1,size(this%elems)
+     do jty=1,size(this%elems)
+        write(unit=iunit,fmt='(a5,a5 $)') & 
+           ',    ',this%elems(ity)%str//'-'//this%elems(jty)%str
+     enddo; enddo
+     write(unit=iunit,fmt=*)
+
+     do kk=1, size(this%sq,dim=3)
+
+        dqk = DQ*kk
+        write(unit=iunit,fmt='(f12.5 $)') dqk
+
+        do ity=1,size(this%elems)
+        do jty=1,size(this%elems)
+
+           ! initialize sq for the r-value integral 
+           this%sq(ity,jty,kk) = 0.d0
+
+           do k=1,size(this%gr,dim=3)
+              dr = k/DRI
+              prefactor = (this%gr(ity,jty,k)-1.d0)*cos(0.5d0*pi*dr/RCUT) ! (gr - 1)*taper
+              this%sq(ity,jty,kk) = this%sq(ity,jty,kk) + &
+                  dr*dr*prefactor*sin(dr*dqk)/(dr*dqk)/DRI
+           enddo
+
+           ! get prefactor 
+           prefactor = 4d0*pi*rho*sqrt(this%concentration(ity)*this%concentration(jty))
+           this%sq(ity,jty,kk) = this%sq(ity,jty,kk)*prefactor
+
+           ! add delta function if ity==jty
+           if(ity==jty) this%sq(ity,jty,kk) = this%sq(ity,jty,kk) + 1.d0
+           write(iunit, fmt='(f12.5,3x $)') this%sq(ity,jty,kk)
+
+        enddo; enddo
+        write(unit=iunit,fmt=*)
+
+     enddo
+     close(iunit)
+
+  end subroutine
+
+  function get_analysis_context_from_mdframe(oneframe) result(c)
+     type(mdframe),intent(in) :: oneframe
+     type(analysis_context) :: c
+     integer :: i, ne, ity, jty, kty, idx
+
+     c%num_atoms = oneframe%num_atoms
+     c%lattice = oneframe%lattice
+     c%volume = oneframe%lattice(1)*oneframe%lattice(2)*oneframe%lattice(3)
+
+     allocate(c%elems(0))
+     do i=1, size(oneframe%elems)
+        if( get_index(c%elems,oneframe%elems(i)%str) < 0 ) &
+            c%elems = [c%elems, oneframe%elems(i)]
+     enddo
+  
+     c%kvector(1:3)=2d0*pi/c%lattice(1:3)
+
+     ne = size(c%elems)
+     c%num_atom_types = ne
+
+     allocate(c%gr(ne,ne,NTABLES),c%nr(ne,ne,NTABLES),c%sq(ne,ne,NTABLES),c%ba(ne,ne,ne,NTABLES))
+     c%gr=0.d0;  c%nr=0.d0;  c%sq=0.d0; c%ba=0.d0
+
+     allocate(c%concentration(ne), c%num_atoms_per_type(ne))
+     c%concentration=0.0d0; c%num_atoms_per_type=0
+
+     do i=1, size(oneframe%elems)
+        idx = get_index(c%elems, oneframe%elems(i)%str) 
+        if(idx>0) then 
+            c%num_atoms_per_type(idx) = c%num_atoms_per_type(idx)+1
+        else
+            print'(a,a,i6)', 'ERROR: uncategorized atom found: in ', oneframe%filename, i
+        endif
+     enddo
+
+     c%concentration = dble(c%num_atoms_per_type)/c%num_atoms
+
+     c%num_sample_frames = 0
+
+     call c%print()
+  end function
+
+  function get_index(elems, name) result(idx)
+    type(string_array),allocatable,intent(in) :: elems(:)
+    character(len=:),allocatable,intent(in) :: name
+    integer :: idx
+
+    do idx = 1, size(elems)
+       if (elems(idx)%str==name) return 
+    enddo
+    idx = -1
+
+    return
+  end
+
+end module
